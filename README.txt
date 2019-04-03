978aa224d5150b59527643f9056eb1a0abf7f46a
pratibhashrestha <pratibha.shrest@gmail.com>
2019-04-02 17:43:50 -0700

    Added clearer notes to readme and py file

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
 Python 3 was used to run the Python code as I was having problems with the terminal
 I used "https://docs.python.org/3/tutorial/inputoutput.html" to print the outputs in a more clean manner
 
-**There are no futher notes
+********There are no futher notes**********
--- bikeshare_2_pratibhashrestha.py
+++ bikeshare_2_pratibhashrestha.py
@@ -14,6 +14,8 @@ def get_filters():
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
+
+        returns stats for the city month and day
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
